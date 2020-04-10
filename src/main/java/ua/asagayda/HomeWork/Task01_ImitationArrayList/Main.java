817ed054752c75e5aed65358cfaa612f0a62e6db
asa-home <sagaidaalexandr@gmail.com>
2020-04-10 08:02:52 +0300

    Edit Main

--- src/main/java/ua/asagayda/HomeWork/Task01_ImitationArrayList/Main.java
+++ src/main/java/ua/asagayda/HomeWork/Task01_ImitationArrayList/Main.java
@@ -5,55 +5,30 @@ class Main {
 
     public static void main(String[] args) {
 
-
-        ListImitation imitation = new ListImitation(12);
-
-        imitation.
-//		imitation.createList(arrTest2, 12);
-//		1) как сделать метод, что бы он возвращал массив с заданным именем и нужной длинной? 
-//		подозреваю что это должен быть инстанс объект, возможно должен передаваться через конструктор.
-
-
+        ListImitation imitation = new ListImitation(3);
+        imitation.addSymbol(1);
+        imitation.listOutput();
+        imitation.addSymbol(2);
+        imitation.listOutput();
+        imitation.addSymbol(3);
+        imitation.listOutput();
+        imitation.addSymbol(4);
+        imitation.listOutput();
+        imitation.addSymbol(5);
+        imitation.listOutput();
+        imitation.addSymbol(6);
+        imitation.listOutput();
+        imitation.addSymbol(7);
+        imitation.listOutput();
+        imitation.addSymbol(8);
+        imitation.listOutput();
+        imitation.addSymbol(9);
+        imitation.listOutput();
+        imitation.addSymbol(10);
+        imitation.listOutput();
+        imitation.addSymbol(11);
         imitation.listOutput();
 
-        imitation.addSymbolToList(1);
-        imitation.addSymbolToList(2);
-        imitation.addSymbolToList(3);
-        imitation.addSymbolToList(4);
-        imitation.addSymbolToList(5);
-        imitation.addSymbolToList(6);
-        imitation.addSymbolToList(7);
-        imitation.addSymbolToList(8);
-        imitation.addSymbolToList(9);
-
-        imitation.listOutput();
-
-
-        imitation.changeElementByIndex(0, 100);
-        imitation.listOutput();
-
-
-        imitation.deleteElementByIndex(0);
-        imitation.listOutput();
-
-//		2) не меняется ссылка на объект который был локальным. 
-//		наверное так же наверное нужно как то внедрять инстанс объект.  
-        imitation.addLength(4);
-        System.out.println("arrTest    addLength = ");
-
-        imitation.listOutput();
-//		2) не меняется ссылка на объект который был локальным. 
-//		наверное так же наверное нужно как то внедрять инстанс объект.
-
-        imitation.decreaseLength(4);
-
-        System.out.println("arrTest setNewLength = ");
-//		2) не меняется ссылка на объект который был локальным. 
-//		наверное так же наверное нужно как то внедрять инстанс объект.
-
-        imitation.listOutput();
-
-        imitation.printListRevers();
 
 
     }
