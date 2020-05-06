a10f5289d6635bf04252a219cce6ea3c067c0d98
asa-home <sagaidaalexandr@gmail.com>
2020-05-06 15:43:38 +0300

    Inheritance2

---
+++ src/main/java/ua/asagayda/homework/task04/reg/ex/StaticTest.java
@@ -0,0 +1,24 @@
+package ua.asagayda.homework.task04.reg.ex;
+
+public class StaticTest {
+    private static int i;
+
+    public StaticTest() {
+        i++;
+    }
+
+    public static int getI() {
+        return i;
+    }
+
+    public static void setI(int i) {
+        StaticTest.i = i;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println(StaticTest.getI());
+
+    }
+}
