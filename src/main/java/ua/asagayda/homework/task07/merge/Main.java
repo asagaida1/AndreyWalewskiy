dd445718dacb9be6ac705e5b4c2c3a1aaed708c4
asa-home <sagaidaalexandr@gmail.com>
2020-06-04 20:36:02 +0300

    add Vehicles 02.06.2020;

--- src/main/java/ua/asagayda/homework/task07/merge/Main.java
+++ src/main/java/ua/asagayda/homework/task07/merge/Main.java
@@ -2,6 +2,6 @@ package ua.asagayda.homework.task07.merge;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main in Sub123 branch");
+        System.out.println("Main in Sub9999 branch");
     }
 }
