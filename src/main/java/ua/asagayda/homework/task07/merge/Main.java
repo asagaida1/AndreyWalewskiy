592aa985ddac0e8912bf35cb0a82b4bd87ce6d5f
asagaida1 <sagaidaalexandr@gmail.com>
2020-07-01 14:08:37 +0300

    Edit Project on the Dacha3.

--- src/main/java/ua/asagayda/homework/task07/merge/Main.java
+++ src/main/java/ua/asagayda/homework/task07/merge/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main in testMergeSub2 branch");
         System.out.println("Test Dacha");
+        System.out.println("Test Dacha");
+        System.out.println("Test Dacha");
 
     }
 }
