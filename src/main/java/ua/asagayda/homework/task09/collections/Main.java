a5af0c277aa246fe7c1c04b1406d66cea86124fc
asagaida1 <sagaidaalexandr@gmail.com>
2020-07-12 15:51:43 +0300

    Dacha 12.07.20 - 2 before the lesson;

---
+++ src/main/java/ua/asagayda/homework/task09/collections/Main.java
@@ -0,0 +1,9 @@
+package ua.asagayda.homework.task09.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
